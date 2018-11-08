80f36d86d33e8722a2ded8425d4f0dba23dd6abf
Vinnys Lins <vinnysfl@hotmail.com>
2018-11-07 23:29:10 -0200

    Implementing Login page

---
+++ app/src/pages/login/login.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  public email: string;
+  public password: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  login(): void {
+    localStorage.setItem('token', 'token');
+    this.navCtrl.push(TabsPage);
+  }
+}
