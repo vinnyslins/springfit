5ead8d154e6ee8a3c460736e2d4f550ea72d4781
Vinnys Lins <vinnysfl@hotmail.com>
2018-11-22 01:30:21 -0200

    Implementing daily training list

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -7,9 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   public percent = 82;
+  public exercises: any[] = [
+    { name: 'Agachamento fundo', series: 10, repeats: 2 },
+    { name: 'Flexão de braços', series: 30, repeats: 2 },
+    { name: 'Abdominal crunch', series: 20, repeats: 5 },
+    { name: 'Tríceps testa', series: 20, repeats: 3 },
+    { name: 'Puxada pulley pela frente', series: 15, repeats: 2 }
+  ];
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  toExercise(exercise: any) {
+    exercise.done = true;
+  }
 }
