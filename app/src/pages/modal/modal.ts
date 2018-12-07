8826c659cbd0d46cb53c6bfdd0e8572fd07003c8
Vinnys Lins <vinnysfl@hotmail.com>
2018-12-06 23:26:25 -0200

    Recuperando dados dos treinos e implementando criação de prática

--- app/src/pages/modal/modal.ts
+++ app/src/pages/modal/modal.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 import { TrainProvider } from '../../providers/train';
 import { UserProvider } from '../../providers/user';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'page-modal',
@@ -10,16 +11,30 @@ import { UserProvider } from '../../providers/user';
 export class ModalPage {
   public learner: any;
   public trains: any[];
+  public exercises: any[];
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController,
     public trainProvider: TrainProvider,
-    public userProvider: UserProvider
+    public userProvider: UserProvider,
+    private alertCtrl: AlertController
   ) {
     this.learner = navParams.get('learner');
     this.trains = this.getWeek();
+
+    this.trainProvider.getTrains().then(response => {
+      const trains = response.filter(train => train.user.userId === this.learner.userId);
+      this.trains.forEach(day => {
+        const train = trains.find(element => element.date == day.date);
+        if (train) day.idTrain = train.idTrain;
+      });
+    });
+
+    this.trainProvider.getExercises().then(response => {
+      this.exercises = response;
+    });
   }
 
   getWeek(): Date[] {
@@ -28,8 +43,8 @@ export class ModalPage {
     current.setDate((current.getDate() - current.getDay()));
     for (let i = 0; i < 7; i++) {
       week.push({
-        date: new Date(current),
-        pratices: []
+        date: new Date(current).toISOString().slice(0, 10),
+        practices: []
       });
       current.setDate(current.getDate() +1);
     }
@@ -43,14 +58,91 @@ export class ModalPage {
   createTrain(date: Date): void {
     const payload = {
       date: date,
-      instructor: { instructorId: this.userProvider.user.userId },
-      learner: { learnerId: this.learner.userId }
+      user: this.learner
     };
 
     this.trainProvider.createTrain(payload).then(response => {
-      console.log(response);
-    }).catch(error => {
-      console.log(error);
+      swal({
+        title: 'Sucesso',
+        text: 'Treino criado com sucesso.',
+        type: 'success',
+        showCloseButton: true,
+        showConfirmButton: false
+      });
+      this.trains.find(train => train.date == response.date).idTrain = response.idTrain;
+    }).catch(() => {
+      swal({
+        title: 'Erro',
+        text: 'Ocorreu um erro ao criar treino.',
+        type: 'error',
+        showCloseButton: true,
+        showConfirmButton: false
+      });
     });
+  }
+
+  createPractice(train: any): void {
+    const payload: any = {
+      train: train,
+      done: false
+    };
+
+    const seriesPrompt = this.alertCtrl.create({
+      title: 'Repetições e séries',
+      inputs: [
+        { type: 'number', name: 'repeats', placeholder: 'Repetições' },
+        { type: 'number', name: 'series', placeholder: 'Séries' }
+      ],
+      buttons: [
+        { text: 'Cancelar' },
+        {
+          text: 'Salvar',
+          handler: data => {
+            if (!data.repeats || !data.series) return false;
+            payload.repeats = data.repeats;
+            payload.series = data.series;
+            this.trainProvider.createPractice(payload).then(response => {
+              train.practices.push(response);
+              swal({
+                title: 'Sucesso',
+                text: 'Prática criada com sucesso.',
+                type: 'success',
+                showCloseButton: true,
+                showConfirmButton: false
+              });
+            }).catch(() => {
+              swal({
+                title: 'Erro',
+                text: 'Ocorreu um erro ao criar prática.',
+                type: 'error',
+                showCloseButton: true,
+                showConfirmButton: false
+              });
+            });
+          }
+        }
+      ]
+    });
+
+    const exercisePrompt = this.alertCtrl.create({
+      title: 'Exercício',
+      buttons: [
+        { text: 'Cancelar' },
+        {
+          text: 'Salvar',
+          handler: data => {
+            if (!data) return false;
+            payload.exercise = { idExercise: data };
+            seriesPrompt.present();
+          }
+        }
+      ]
+    });
+
+    this.exercises.forEach(exercise => {
+      exercisePrompt.addInput({ type: 'radio', label: exercise.name, value: exercise.idExercise });
+    });
+
+    exercisePrompt.present();
   }
 }
--- app/src/providers/train.ts
+++ app/src/providers/train.ts
@@ -11,4 +11,16 @@ export class TrainProvider {
   createTrain(payload: any): Promise<any> {
     return this.http.post(`${this.apiUrl}/train`, payload).toPromise();
   }
+
+  getTrains(): Promise<any> {
+    return this.http.get(`${this.apiUrl}/trains`).toPromise();
+  }
+
+  createPractice(payload: any): Promise<any> {
+    return this.http.post(`${this.apiUrl}/practice`, payload).toPromise();
+  }
+
+  getExercises(): Promise<any> {
+    return this.http.get(`${this.apiUrl}/exercises`).toPromise();
+  }
 }
